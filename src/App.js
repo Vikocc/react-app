e087d28c725a3f770c1f262c2fd5b8b67df39f5d
Vikocc <vikki.lazorenko@gmail.com>
2022-10-30 22:53:07 +0200

    react-weather-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import "./index.css";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="all">
+      <div className="container">
+        <form className="search">
+          <div className="d-flex justify-content-between">
+            <input
+              type="search"
+              className="form-control  m-3"
+              placeholder="Search a city..."
+              autofocus="on"
+              autocomplete="off"
+            />
+            <input
+              type="submit"
+              value="Search"
+              className="form-control btn btn-primary w-50 m-3"
+            />
+          </div>
+        </form>
+        <div className="row details">
+          <h1>New York</h1>
+          <small>
+            Last updated:
+            <br />
+            <span></span>
+          </small>
+          <div className="col-6">
+            <div className="d-flex">
+              <img src="" alt="weather" />
+              <div>
+                <p> </p>
+                <span>
+                  <button className="active">°C</button>|
+                  <button href="#">°F</button>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <p>
+              Humidity: <span></span>%
+            </p>
+            <p>
+              Wind: <span></span> m/s
+            </p>
+            <p></p>
+          </div>
+        </div>
+      </div>
+      <div className="forecast"></div>
+      <div>
+        <p className="gitHub">
+          <a href="https://github.com/Vikocc/weather">Open-sourced on GitHub</a>{" "}
+          made by Vikki with ❤
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
-
-export default App;
